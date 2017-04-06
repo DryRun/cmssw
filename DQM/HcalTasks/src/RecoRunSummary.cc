7a2cfc980bafe50f9a371f6f2615418c558339aa
David Yu <david_yu@brown.edu>
2017-04-06 15:40:12 +0200

    Move offline summary maps to Subdet instead of FED

--- DQM/HcalTasks/src/RecoRunSummary.cc
+++ DQM/HcalTasks/src/RecoRunSummary.cc
@@ -39,36 +39,14 @@ namespace hcaldqm
 			return std::vector<flag::Flag>();
 
 		// FILTERS, some useful vectors, hash maps
-		std::vector<uint32_t> vhashFEDHF;
-		vhashFEDHF.push_back(HcalElectronicsId(22, SLOT_uTCA_MIN,
-			FIBER_uTCA_MIN1, FIBERCH_MIN, false).rawId());
-		vhashFEDHF.push_back(HcalElectronicsId(29, SLOT_uTCA_MIN,
-			FIBER_uTCA_MIN1, FIBERCH_MIN, false).rawId());
-		vhashFEDHF.push_back(HcalElectronicsId(32, SLOT_uTCA_MIN,
-			FIBER_uTCA_MIN1, FIBERCH_MIN, false).rawId());
-		vhashFEDHF.push_back(HcalElectronicsId(22, SLOT_uTCA_MIN+6,
-			FIBER_uTCA_MIN1, FIBERCH_MIN, false).rawId());
-		vhashFEDHF.push_back(HcalElectronicsId(29, SLOT_uTCA_MIN+6,
-			FIBER_uTCA_MIN1, FIBERCH_MIN, false).rawId());
-		vhashFEDHF.push_back(HcalElectronicsId(32, SLOT_uTCA_MIN+6,
-			FIBER_uTCA_MIN1, FIBERCH_MIN, false).rawId());
-		filter::HashFilter filter_FEDHF;
-		filter_FEDHF.initialize(filter::fPreserver, hashfunctions::fFED,
-			vhashFEDHF);    // preserve only HF FEDs
+		std::vector<uint32_t> vhashHF;
+		vhashHF.push_back(HcalDetId(HcalForward, 1, 1, 1).rawId());
+		filter::HashFilter filter_HF;
+		filter_HF.initialize(filter::fPreserver, hashfunctions::fSubdet, vhashHF);    // preserve only HF
+
 		electronicsmap::ElectronicsMap ehashmap;
 		ehashmap.initialize(_emap, electronicsmap::fD2EHashMap);
 		bool tcdsshift = false;
-		filter::HashFilter filter_VME;
-		filter::HashFilter filter_uTCA;
-		std::vector<uint32_t> vhashVME,vhashuTCA;
-		vhashVME.push_back(HcalElectronicsId(constants::FIBERCH_MIN,
-			constants::FIBER_VME_MIN, SPIGOT_MIN, CRATE_VME_MIN).rawId());
-		vhashuTCA.push_back(HcalElectronicsId(CRATE_uTCA_MIN, SLOT_uTCA_MIN,
-			FIBER_uTCA_MIN1, FIBERCH_MIN, false).rawId());
-		filter_uTCA.initialize(filter::fFilter, hashfunctions::fElectronics,
-			vhashuTCA);
-		filter_VME.initialize(filter::fFilter, hashfunctions::fElectronics,
-			vhashVME);
 		std::vector<flag::Flag> vflags; vflags.resize(nRecoFlag);
 		vflags[fUniSlotHF]=flag::Flag("UniSlotHF");
 		vflags[fTCDS]=flag::Flag("TCDS");
@@ -102,7 +80,7 @@ namespace hcaldqm
 			new quantity::ValueQuantity(quantity::fState),0);
 
 		//	BOOK
-		xUniHF.book(_emap, filter_FEDHF);
+		xUniHF.book(_emap, filter_HF);
 
 
 		//	LOAD
@@ -165,31 +143,22 @@ namespace hcaldqm
 		//	summary flags
 		std::vector<flag::Flag> sumflags;
 		int ifed=0;
-		for (std::vector<uint32_t>::const_iterator it=_vhashFEDs.begin();
-			it!=_vhashFEDs.end(); ++it)
+		for (std::vector<uint32_t>::const_iterator it=_vhashSubdets.begin();
+			it!=_vhashSubdets.end(); ++it)
 		{
 			flag::Flag fSum("RECO");
 			HcalElectronicsId eid(*it);
-
-			std::vector<uint32_t>::const_iterator cit=std::find(
-				_vcdaqEids.begin(), _vcdaqEids.end(),*it);
-			if (cit==_vcdaqEids.end())
-			{
-				//	not registered @cDAQ
-				sumflags.push_back(flag::Flag("RECO", flag::fNCDAQ));
-				ifed++;
-				continue;
-			}
+			HcalDetId did(*it);
 
 			//	registered @cDAQ
-			if (utilities::isFEDHBHE(eid))
+			if (did.subdet() == HcalBarrel || did.subdet() == HcalEndcap)
 			{
 				if (tcdsshift)
 					vflags[fTCDS]._state = flag::fBAD;
 				else
 					vflags[fTCDS]._state = flag::fGOOD;
 			}
-			if (utilities::isFEDHF(eid))
+			if (did.subdet() == HcalForward)
 			{
 				if (xUni.get(eid)>0)
 					vflags[fUniSlotHF]._state = flag::fBAD;
