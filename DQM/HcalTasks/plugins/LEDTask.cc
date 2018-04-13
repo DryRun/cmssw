daa15d055d674e4ad10b61a692a7630af7ec42e1
David <david.renhwa.yu@gmail.com>
2018-04-13 16:14:32 +0200

    Try to get LEDTask to merge

--- DQM/HcalTasks/plugins/LEDTask.cc
+++ DQM/HcalTasks/plugins/LEDTask.cc
@@ -19,16 +19,15 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 	_tagTrigger = ps.getUntrackedParameter<edm::InputTag>("tagTrigger",
 		edm::InputTag("tbunpacker"));
 	_tokHBHE = consumes<HBHEDigiCollection>(_tagHBHE);
-	_tokHE = consumes<QIE11DigiCollection>(_tagHE);
+	_tokHEP17 = consumes<QIE11DigiCollection>(_tagHE);
 	_tokHO = consumes<HODigiCollection>(_tagHO);
 	_tokHF = consumes<QIE10DigiCollection>(_tagHF);
 	_tokTrigger = consumes<HcalTBTriggerData>(_tagTrigger);
-	_tokuMN = consumes<HcalUMNioDigi>(_taguMN);
 
 	//	constants
 	_lowHBHE = ps.getUntrackedParameter<double>("lowHBHE",
 		20);
-	_lowHE = ps.getUntrackedParameter<double>("lowQIE11",
+	_lowHEP17 = ps.getUntrackedParameter<double>("lowHEP17",
 		20);
 	_lowHO = ps.getUntrackedParameter<double>("lowHO",
 		20);
@@ -165,24 +164,7 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 			new hcaldqm::quantity::ElectronicsQuantity(hcaldqm::quantity::fFiberuTCAFiberCh),
 			new hcaldqm::quantity::ValueQuantity(hcaldqm::quantity::fN),0);
 	}
-
-	// Plots for LED in global
-	if (_ptype == fOnline) {
-		_cADCvsTS_SubdetPM.initialize(_name, "ADCvsTS",
-			hcaldqm::hashfunctions::fSubdetPM,
-			new hcaldqm::quantity::ValueQuantity(hcaldqm::quantity::fTiming_TS),
-			new hcaldqm::quantity::ValueQuantity(hcaldqm::quantity::fQIE10ADC_256),
-			new hcaldqm::quantity::ValueQuantity(hcaldqm::quantity::fN, true),0);
-		_cSumQ_SubdetPM.initialize(_name, "SumQ", 
-			hcaldqm::hashfunctions::fSubdetPM,
-			new hcaldqm::quantity::ValueQuantity(hcaldqm::quantity::fQIE10fC_400000),
-			new hcaldqm::quantity::ValueQuantity(hcaldqm::quantity::fN, true),0);
-		_cTDCTime_SubdetPM.initialize(_name, "SumQ", 
-			hcaldqm::hashfunctions::fSubdetPM,
-			new hcaldqm::quantity::ValueQuantity(hcaldqm::quantity::fQIE10fC_400000),
-			new hcaldqm::quantity::ValueQuantity(hcaldqm::quantity::fN, true),0);
-	}
-
+	
 	//	initialize compact containers
 	_xSignalSum.initialize(hcaldqm::hashfunctions::fDChannel);
 	_xSignalSum2.initialize(hcaldqm::hashfunctions::fDChannel);
@@ -214,12 +196,6 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 		_cShapeCut_FEDSlot.book(ib, _emap, _subsystem);
 		_cMissing_FEDVME.book(ib, _emap, _filter_uTCA, _subsystem);
 		_cMissing_FEDuTCA.book(ib, _emap, _filter_VME, _subsystem);
-	}
-	if (_ptype == fOnline) {
-
-		_cADCvsTS_SubdetPM.book(ib, _emap, _subsystem);
-		_cSumQ_SubdetPM.book(ib, _emap, _subsystem);
-		_cTDCTime_SubdetPM.book(ib, _emap, _subsystem);
 	}
 
 	_xSignalSum.book(_emap);
@@ -317,7 +293,7 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 	edm::Handle<HBHEDigiCollection>		chbhe;
 	edm::Handle<HODigiCollection>		cho;
 	edm::Handle<QIE10DigiCollection>		chf;
-	edm::Handle<QIE11DigiCollection>		che;
+	edm::Handle<QIE11DigiCollection>		chep17;
 
 	if (!e.getByToken(_tokHBHE, chbhe))
 		_logger.dqmthrow("Collection HBHEDigiCollection isn't available "
@@ -328,7 +304,7 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 	if (!e.getByToken(_tokHF, chf))
 		_logger.dqmthrow("Collection QIE10DigiCollection isn't available "
 			+ _tagHF.label() + " " + _tagHF.instance());
-	if (!e.getByToken(_tokHE, che))
+	if (!e.getByToken(_tokHEP17, chep17))
 		_logger.dqmthrow("Collection QIE11DigiCollection isn't available "
 			+ _tagHE.label() + " " + _tagHE.instance());
 
@@ -357,22 +333,15 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 		_xTimingSum2.get(did)+=aveTS*aveTS;
 		_xEntries.get(did)++;
 
-		if (_ptype == fLocal) { // hidefed2crate
+		if (_ptype != fOffline) { // hidefed2crate
 			for (int i=0; i<digi.size(); i++) {
 				//_cShapeCut_FEDSlot.fill(eid, i, digi.sample(i).nominal_fC()-2.5);
 				_cShapeCut_FEDSlot.fill(eid, i, hcaldqm::utilities::adc2fCDBMinusPedestal<HBHEDataFrame>(_dbService, digi_fC, did, digi, i));
 			}
 		}
-
-		if (_ptype == fOnline) {
-			for (int iTS = 0; iTS < digi.size(); ++iTS) {
-				_cADCvsTS_SubdetPM.fill(did, iTS, digi.sample(iTS).adc());					
-			}
-			_cSumQ_SubdetPM.fill(did, sumQ);
-		}
 	}
 
-	for (QIE11DigiCollection::const_iterator it=che->begin(); it!=che->end();
+	for (QIE11DigiCollection::const_iterator it=chep17->begin(); it!=chep17->end();
 		++it)
 	{
 		const QIE11DataFrame digi = static_cast<const QIE11DataFrame>(*it);
@@ -394,7 +363,7 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 		//double sumQ = hcaldqm::utilities::sumQ_v10<QIE11DataFrame>(digi, 2.5, 0, digi.samples()-1);
 		double sumQ = hcaldqm::utilities::sumQDB<QIE11DataFrame>(_dbService, digi_fC, did, digi, 0, digi.samples()-1);
 		if (sumQ<_lowHEP17)
-		continue;
+			continue;
 
 		//double aveTS = hcaldqm::utilities::aveTS_v10<QIE11DataFrame>(digi, 2.5, 0,digi.samples()-1);
 		double aveTS = hcaldqm::utilities::aveTSDB<QIE11DataFrame>(_dbService, digi_fC, did, digi, 0, digi.size()-1);
@@ -405,21 +374,11 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 		_xTimingSum2.get(did)+=aveTS*aveTS;
 		_xEntries.get(did)++;
 
-		if (_ptype == fLocal) { // hidefed2crate
+		if (_ptype != fOffline) { // hidefed2crate
 			for (int i=0; i<digi.samples(); i++) {
 				//_cShapeCut_FEDSlot.fill(eid, i, digi.sample(i).nominal_fC()-2.5);
 				_cShapeCut_FEDSlot.fill(eid, i, hcaldqm::utilities::adc2fCDBMinusPedestal<QIE11DataFrame>(_dbService, digi_fC, did, digi, i));
 			}
-		}
-		if (_ptype == fOnline) {
-			for (unsigned int iTS = 0; iTS < digi.size(); ++iTS) {
-				_cADCvsTS_SubdetPM.fill(did, iTS, digi[iTS].adc());					
-				if (digi[iTS].tdc() <50) {
-					double time = iTS*25. + (digi[iTS].tdc() / 2.);
-					_cTDCTime_SubdetPM.fill(did, time);
-				}
-			}
-			_cSumQ_SubdetPM.fill(did, sumQ);
 		}
 	}
 	for (HODigiCollection::const_iterator it=cho->begin();
@@ -443,20 +402,13 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 		_xTimingSum2.get(did)+=aveTS*aveTS;
 		_xEntries.get(did)++;
 
-		if (_ptype == fLocal) { // hidefed2crate
+		if (_ptype != fOffline) { // hidefed2crate
 			for (int i=0; i<digi.size(); i++) {
 				//_cShapeCut_FEDSlot.fill(eid, i, digi.sample(i).nominal_fC()-8.5);
 				_cShapeCut_FEDSlot.fill(eid, i, hcaldqm::utilities::adc2fCDBMinusPedestal<HODataFrame>(_dbService, digi_fC, did, digi, i));
 			}
 		}
-		if (_ptype == fOnline) {
-			for (int iTS = 0; iTS < digi.size(); ++iTS) {
-				_cADCvsTS_SubdetPM.fill(did, iTS, digi.sample(iTS).adc());					
-			}
-			_cSumQ_SubdetPM.fill(did, sumQ);
-		}
 	}
-
 	for (QIE10DigiCollection::const_iterator it=chf->begin();
 		it!=chf->end(); ++it)
 	{
@@ -481,23 +433,13 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 		_xTimingSum2.get(did)+=aveTS*aveTS;
 		_xEntries.get(did)++;
 
-		if (_ptype == fLocal) { // hidefed2crate
+		if (_ptype != fOffline) { // hidefed2crate
 			for (int i = 0; i < digi.samples(); ++i) {
 				// Note: this used to be digi.sample(i).nominal_fC() - 2.5, but this branch doesn't exist in QIE10DataFrame.
 				// Instead, use lookup table.
 				//_cShapeCut_FEDSlot.fill(eid, i, constants::adc2fC[digi[i].adc()]);
 				_cShapeCut_FEDSlot.fill(eid, i, hcaldqm::utilities::adc2fCDBMinusPedestal<QIE10DataFrame>(_dbService, digi_fC, did, digi, i));
 			}
-		}
-		if (_ptype == fOnline) {
-			for (unsigned int iTS = 0; iTS < digi.size(); ++iTS) {
-				_cADCvsTS_SubdetPM.fill(did, iTS, digi[iTS].adc());					
-				if (digi[iTS].le_tdc() <50) {
-					double time = iTS*25. + (digi[iTS].le_tdc() / 2.);
-					_cTDCTime_SubdetPM.fill(did, time);
-				}
-			}
-			_cSumQ_SubdetPM.fill(did, sumQ);
 		}
 	}
 
@@ -516,15 +458,8 @@ LEDTask::LEDTask(edm::ParameterSet const& ps):
 			_logger.dqmthrow("Collection HcalTBTriggerData isn't available "
 				+ _tagTrigger.label() + " " + _tagTrigger.instance());
 		return ctrigger->wasLEDTrigger();
-	} else {
-		//	fOnline mode
-		edm::Handle<HcalUMNioDigi> cumn;
-		if (!e.getByToken(_tokuMN, cumn)) {
-			return false;
-		}
-		
-		return (cumn->eventType() == constants::EVENTTYPE_LED);
 	}
+
 	return false;
 }
 
